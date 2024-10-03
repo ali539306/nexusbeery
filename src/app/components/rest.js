39798e868268acd7b35c76e0fc10ac48882f963f
ali539306 <su92-bscsm-f23-367@superior.edu.pk>
2024-10-03 09:13:46 -0700

    final

---
+++ src/app/components/rest.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "@/style/style.css"
+export default function Rest() {
+
+    
+
+    
+  return (
+   <>
+    <div className="rest">
+        <h4 className='heading'>Geography & Travel</h4>
+        <div className="back">
+            <div className="small-card">Geography & Travel</div>
+            <div className="large-card">
+                <h2 className="us">U.S. State Capitals Quiz</h2>
+                <button className="start" ><a href="/Quiz">start</a></button>
+            </div>
+        </div>
+    </div>
+   </>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/nav';
+import Rest from './components/rest';
+
+export default function Home()
+{
+  return(
+    <>
+   <Navbar/>
+   <Rest/>
+   
+         </>
+  )
+}
